66494af0e0016c94389dc91a7d0f1ac93b0b1ee0
Antoine Boucher <antoine.bou13@gmail.com>
2024-11-29 12:49:39 -0500

    Delete Simulation directory

--- Simulation/simulation/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .parameters import Parameters
-from .potential import Potential
-from .gradient import Gradient
-from .hessian import Hessian
-
-__all__ = ['Parameters', 'Potential', 'Gradient', 'Hessian']
